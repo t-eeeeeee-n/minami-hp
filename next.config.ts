2a8466967168c7cc4961811ac766ad29cba2a5af
tensho.arai <tensho.arai@ities-inc.co.jp>
2025-01-23 00:45:52 +0900

    fix: docker

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    webpack: (config) => {
+        config.watchOptions = {
+            poll: 1000, // 1秒ごとにポーリング
+            aggregateTimeout: 300, // 300ms待機してから再ビルド
+        };
+        return config;
+    },
     images: {
         remotePatterns: [
             {
