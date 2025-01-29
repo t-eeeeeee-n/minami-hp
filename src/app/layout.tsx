06ba466eabcc57fd013b3efd129e703fdd074339
t-eeeeeee-n <t.eeee.n.nir@gmail.com>
2025-01-29 15:31:01 +0900

    fix: rich result

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import '@/styles/globals.css';
 import '@/styles/globalicons.css';
 import '@/styles/globalFont.css'
+
 import { Metadata } from 'next';
 import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import {Toaster} from "react-hot-toast";
 import {DEFAULT_METADATA} from "@/constants/metadata";
-import Head from "next/head";
 import {DEFAULT_SCHEMA_ORG_JSON} from "@/constants/schema";
 
 export const metadata: Metadata = DEFAULT_METADATA;
@@ -16,9 +16,9 @@ export default function RootLayout({children,}: { children: React.ReactNode;
 }) {
     return (
         <html lang="ja">
-        <Head>
-            <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(DEFAULT_SCHEMA_ORG_JSON) }} />
-        </Head>
+            <head>
+                <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(DEFAULT_SCHEMA_ORG_JSON) }} />
+            </head>
             <body>
                 <Toaster
                     position="top-center"
