637951cb8c8f9010a469f7241aef564323adc014
t-eeeeeee-n <t.eeee.n.nir@gmail.com>
2025-05-07 13:40:47 +0900

    fix: change campaign

--- src/components/Campaign.tsx
+++ src/components/Campaign.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { event } from "@/lib/gtag";
 
 const Campaign = () => {
